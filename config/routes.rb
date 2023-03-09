ea92d54761d1597f7ab8a5532d6f471dee5ba718
Daniel Lameira <dlameira@gmail.com>
2023-03-09 18:36:41 -0300

    cloudnary and env done

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
   resources :universes do
   end
-end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:show]
+end
