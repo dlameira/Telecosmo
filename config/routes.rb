a917e40f3ea06a89f0c51a2f41014abebc6dbb0b
Joao Toledo <j.toledo.rocha96@gmail.com>
2023-03-09 15:24:06 -0300

    universe show

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :universes do
+  end
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create(email: "joao@gmail.com", password: "123456", nickname: "Example User", first_name: "Example", last_name: "User", birthday: Date.new(1990, 1, 1), bio: "I'm an example user!")
+
