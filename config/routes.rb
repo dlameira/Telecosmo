d25a577b690f432fdaab216099ea3dc2e1d80cc4
Patrick Santos <patrick.sg.santos@gmail.com>
2023-03-10 14:56:32 -0300

    add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :universes do
-  end
+  resources :universes, only: [:index, :show]
   resources :tags, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
