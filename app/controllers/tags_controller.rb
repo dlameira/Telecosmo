7d4522e9287bc2cd41cf4ac85b38684606a376b4
fernando santiago <fer.santiago16@gmail.com>
2023-03-10 12:06:05 -0300

    searchbar working

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,11 @@
 class TagsController < ApplicationController
   def index
-    if params[:query].present?
-      @tags = Tag.where(name: params[:query])
-    else
-      @tags = policy_scope(Tag)
-    end
+    @universes = PgSearch.multisearch(params[:query]) if params[:query].present?
+    @universes = policy_scope(Universe)
+    # if params[:query].present?
+    #   @tags = Tag.where(name: params[:query])
+    # else
+    #   @tags = policy_scope(Tag)
+    # end
   end
 end
--- app/policies/tag_policy.rb
+++ app/policies/tag_policy.rb
@@ -2,7 +2,7 @@ class TagPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-       scope.all
+      scope.all
     end
   end
 end
