9f52e3879c5ab6c4478cfd1388fb67080aba06f6
Joao Toledo <j.toledo.rocha96@gmail.com>
2023-03-11 15:11:27 -0300

    creating new actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     authorize @user
   end
+  def new_friendship
+    if current_user != @user
+      Friendship.create(asker_id: current_user.id, receiver_id: @user.id)
+    end
+    redirect_to @user
+  end
+  def accept_friendship
+    @friendship = Friendship.find(params[:id])
+    @friendship.is_accepted = true
+    @friendship.create(asker_id: friendship.asker_id, receiver_id: friendship.receiver_id)
+    redirect_to current_user
+
+
+
+  end
 end
