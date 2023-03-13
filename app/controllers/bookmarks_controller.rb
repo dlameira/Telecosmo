a48cd752efe8027b374dc427d03537a1ba73c4d6
Patrick Santos <patrick.sg.santos@gmail.com>
2023-03-13 16:39:37 -0300

    constructing the bookmark routes

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,19 @@
 class BookmarksController < ApplicationController
+
+  def create
+    @bookmark = Bookmark.new
+    @universe = Universe.find(bookmark_params)
+    @bookmark.universe = @universe
+    @bookmark.user = current_user
+    authorize @bookmark
+    @bookmark.save!
+    raise
+  end
+
+  private
+
+  def bookmark_params
+    raise
+    params.require(:bookmark).permit(:user_id, :universe_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :universes, only: [:index, :show]
-  resources :tags, only: [:index]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-
 
   get "search", to: "universes#search"
+  resources :universes, only: [:index, :show] do
+    resources :bookmarks, only: [ :create]
+  end
 
+  resources :tags, only: [:index]
   resources :users, only: [:show] do
     member do
       post :add_friend
     end
   end
-
 end
