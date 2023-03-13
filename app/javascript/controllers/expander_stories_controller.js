6cc20abb1b4d54a95ab9b2b79f20120e3c264f8e
Patrick Santos <patrick.sg.santos@gmail.com>
2023-03-13 14:55:00 -0300

    javascript done , need only some improves in the animation

--- app/javascript/controllers/expander_stories_controller.js
+++ app/javascript/controllers/expander_stories_controller.js
@@ -2,29 +2,36 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="expander-stories"
 export default class extends Controller {
-  static targets = ["story", "scontent", "ucontent"]
+  static targets = ["story", "scontent", "ucontent"];
   connect() {
   }
 
   expand(event) {
-    // console.log(this.scontentTargets);
+    const lastElement = this.scontentTargets.find(element => element.classList.contains('story-show'));
 
-    // this.scontentTarget.classList.toggle('story-hyde');
-    // this.scontentTarget.classList.toggle('story-show');
-
-    const teste = this.storyTargets.find(element => element.classList.contains('story-show'))
-    console.log(teste);
-    if (teste != undefined) {
-      teste.classList.remove('story-show');
+    if (event.currentTarget.contains(lastElement)){
+      lastElement.classList.remove('story-show');
+      lastElement.classList.add('story-hyde');
+      this.ucontentTarget.classList.remove('story-hyde');
+      return 0;
     }
 
-    event.currentTarget.classList.add('story-show');
+    this.scontentTargets.forEach(element => {
+      if (element.classList.contains('story-show')){
+        element.classList.remove('story-show')
+        element.classList.add('story-hyde')
+      } else {
+        element.classList.add('story-hyde')
+      }
+    });
 
+     const teste = this.scontentTargets.find(element => event.currentTarget.contains(element));
+     teste.classList.remove('story-hyde');
+     teste.classList.add('story-show');
 
-    // if (this.scontent.classList.contains('story-show')) {
-    //   this.ucontentTarget.classList.add('story-hyde');
-    // } else {
-    //   this.ucontentTarget.classList.remove('story-hyde');
-    // };
+     if (teste != undefined){
+      this.ucontentTarget.classList.add('story-hyde')
+     }
+
   }
 }
