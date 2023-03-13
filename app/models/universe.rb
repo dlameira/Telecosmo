5e4f292491203c8cecddb6006ff3e145923ecd28
Daniel Lameira <dlameira@gmail.com>
2023-03-13 12:51:57 -0300

    seed mais ou menos feito

--- app/models/universe.rb
+++ app/models/universe.rb
@@ -1,11 +1,11 @@
 class Universe < ApplicationRecord
   belongs_to :user
-  has_many :stories
+  has_many :stories, dependent: :destroy
 
   include PgSearch::Model
   pg_search_scope :search_by_title,
-  against: [:title],
-  using: {
-    tsearch: { prefix: true }
-  }
+                  against: [:title],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
