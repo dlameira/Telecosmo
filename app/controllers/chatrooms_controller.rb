6e9d20f7ff152a0157ca1633f6c7f70687b52626
Patrick Santos <patrick.sg.santos@gmail.com>
2023-03-16 12:59:52 -0300

    chatrooms implemented in the user show page

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,19 +1,15 @@
 class ChatroomsController < ApplicationController
 
-
-
-
   def create
-    if Chatroom.between(params[:sender_id], params[:recipient_id]).present?
-      @chatroom = Chatroom.between(params[:sender_id], params[:recipient_id]).first
-    else
-      @chatroom = Chatroom.create!(chatroom_params)
-    end
+    # raise
+    # if Chatroom.between(current_user.id, params[:user_id]).present?
+    #   @chatroom = Chatroom.between(current_user.id, params[:user]).first
+    # else
+    #   @chatroom = Chatroom.create!(sender_id: current_user.id, recipient_id: params[:user])
+    # end
 
     redirect_to chatroom_path(@chatroom)
   end
-
-
 
   def show
     @chatroom = Chatroom.find(params[:id])
@@ -21,17 +17,9 @@ class ChatroomsController < ApplicationController
     @message = Message.new
   end
 
-
   private
 
   def chatroom_params
-    params.require(:chatrooms).permit(:sender_id, :recipient_id)
+    # params.require(:chatroom).permit(:sender_id, :recipient_id, :id)
   end
 end
-
-
-
-
-
-
-
