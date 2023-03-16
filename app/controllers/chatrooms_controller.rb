69b58fcdc90d347812a7f71ab1e06a59b9a3c6d5
Joao Toledo <j.toledo.rocha96@gmail.com>
2023-03-16 11:14:15 -0300

    trying to fix

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,18 +1,19 @@
 class ChatroomsController < ApplicationController
 
 
+
+
   def create
-    if Chatroom.between(params[:sender_id],params[:recipient_id])
-      .present?
-       @chatroom = Chatroom.between(params[:sender_id],
-        params[:recipient_id]).first
+    if Chatroom.between(params[:sender_id], params[:recipient_id]).present?
+      @chatroom = Chatroom.between(params[:sender_id], params[:recipient_id]).first
     else
-        @chatroom = Chatroom.create!(chatroom_params)
+      @chatroom = Chatroom.create!(chatroom_params)
     end
-        redirect_to  chatroom_path(@chatroom)
 
-
+    redirect_to chatroom_path(@chatroom)
   end
+
+
 
   def show
     @chatroom = Chatroom.find(params[:id])
@@ -22,11 +23,12 @@ class ChatroomsController < ApplicationController
 
 
   private
-  def chatroom_params
-    params.permit(:sender_id, :recipient_id)
-   end
 
+  def chatroom_params
+    params.require(:chatrooms).permit(:sender_id, :recipient_id)
+  end
 end
+
 
 
 
