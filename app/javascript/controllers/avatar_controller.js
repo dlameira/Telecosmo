11ad19ac8f0685f534fcb70c471ff9b8d0519301
Patrick Santos <patrick.sg.santos@gmail.com>
2023-03-17 16:46:28 -0300

    javascript done for the first 3 screen to avatar

--- app/javascript/controllers/avatar_controller.js
+++ app/javascript/controllers/avatar_controller.js
@@ -3,8 +3,16 @@ import { createAvatar } from '@dicebear/core';
 import { bigSmile } from '@dicebear/collection';
 
 // Connects to data-controller="avatar"
+
+const persona = []
+const accessories = []
+const eyes = []
+const hair = []
+
+
 export default class extends Controller {
-  static targets = ["newavatarboy", "newavatargirl", "inputOne"]
+  static targets = [ "inputOne", "title", "fase1", "fase2", "fase3"]
+
   connect() {
 
     const boyavat = createAvatar(bigSmile, {
@@ -14,12 +22,34 @@ export default class extends Controller {
       seed: 'Aneka'
     });
 
-    this.boy = boyavat.toString()
-    this.girl = girlavat.toString()
+    // this.boy = boyavat.toString()
+    // this.girl = girlavat.toString()
 
-    this.newavatarboyTarget.insertAdjacentHTML('beforeend', this.boy)
-    this.newavatargirlTarget.insertAdjacentHTML('beforeend', this.girl)
-    this.inputOneTarget.value = this.boy
+    // this.newavatarboyTarget.insertAdjacentHTML('beforeend', this.boy)
+    // this.newavatargirlTarget.insertAdjacentHTML('beforeend', this.girl)
+    // this.inputOneTarget.value = this.boy
+  }
+
+  setpersona(event) {
+    persona.push(event.currentTarget.getAttribute('id'))
+    console.log(persona)
+    this.fase1Target.classList.add('d-none')
+    this.fase2Target.classList.remove("d-none")
+    this.titleTarget.classList.add('d-none')
+  }
+
+  setaccessories(event) {
+    accessories.push(event.currentTarget.getAttribute('id'))
+    console.log(accessories)
+    this.fase2Target.classList.add('d-none')
+    this.fase3Target.classList.remove("d-none")
+  }
+
+  seteyes(event) {
+    eyes.push(event.currentTarget.getAttribute('id'))
+    console.log(eyes)
+    this.fase3Target.classList.add('d-none')
+    this.fase4Target.classList.remove("d-none")
   }
 
 }
